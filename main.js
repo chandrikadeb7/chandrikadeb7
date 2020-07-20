db6517f909e7af0292e126ad1be9413064bd5d8d
Chandrika Deb <chandrikadeb7@gmail.com>
2020-07-20 21:45:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var current = 1; //keeps track of the current div
+var height = $('.roles').height(); //the height of the roles div
+var numberDivs = $('.roles').children().length; //the number of children of the roles div
+var first = $('.roles div:nth-child(1)'); //the first div nested in roles div
+setInterval(function() {
+    var number = current * -height;
+    first.css('margin-top', number + 'px');
+    if (current === numberDivs) {
+        first.css('margin-top', '0px');
+        current = 1;
+    } else current++;
+}, 2000);
